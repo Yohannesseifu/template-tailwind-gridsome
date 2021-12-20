30811d64c8fd1311f8772275f6a79db15d60aa1c
Yohannes Seifu <Yohannesseifu3102@gmail.com>
2021-12-20 13:41:17 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# Default starter for Gridsome
+
+This is the project you get when you run `gridsome create new-project`.
+
+## 1. Install Gridsome CLI tool if you don't have it
+
+`npm install --global @gridsome/cli`
+
+## 2. Create a Gridsome project
+
+1. `gridsome create my-gridsome-site` to install default starter
+2. `cd my-gridsome-site` to open the folder
+3. `gridsome develop` to start a local dev server at `http://localhost:8080`
+4. Happy coding 🎉🙌
+
+[![js-standard-style](https://img.shields.io/badge/code%20style-standard-brightgreen.svg)](http://standardjs.com)
+
+<a href="https://www.buymeacoffee.com/blp" target="_blank"><img src="https://cdn.buymeacoffee.com/buttons/default-blue.png" alt="Buy Me A Coffee" style="height: 51px !important;width: 217px !important;" ></a>
---
+++ gridsome.config.js
@@ -0,0 +1,31 @@
+// This is where project configuration and plugin options are located.
+// Learn more: https://gridsome.org/docs/config
+
+// Changes here require a server restart.
+// To restart press CTRL + C in terminal and run `gridsome develop`
+
+module.exports = {
+  siteName: "Gridsome",
+  settings: {
+    // Place site-wide settings here and query them from the GraphQL layer's metadata {settings} key.
+    likes: {
+      language: "JavaScript",
+      frameworks: ["Vue", "Gridsome", "Tailwind"],
+    },
+  },
+  plugins: [
+    {
+      use: "gridsome-plugin-tailwindcss",
+      options: {
+        shouldImport: true,
+        shouldTimeTravel: true,
+      },
+    },
+    {
+      use: "gridsome-plugin-svg",
+    },
+    {
+      use: "gridsome-plugin-base-components",
+    },
+  ],
+};
